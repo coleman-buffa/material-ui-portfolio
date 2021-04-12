fd2f9fa35a0bc3fd8b64da6279b7a44e706a4522
Coleman Buffa <coleman.buffa@gmail.com>
2021-04-12 11:56:13 -0700

    Create route to retieve all projects. Test with Postman.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ const router = require("express").Router();
 const Project = require("../models/project.js");
 
 //Retrieve all words
-router.get("/api/all/", (req, res) => {});
-
+router.get("/project/all/", (req, res) => {
+  Project.find({})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err)
+    });
+});
 
 module.exports = router;
