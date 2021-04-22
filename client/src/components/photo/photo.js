c156cc4fd66ea23f8cbcf0ba8d5396cfe578cc0c
Coleman Buffa <coleman.buffa@gmail.com>
2021-04-22 16:34:55 -0700

    Create photo component.

--- client/src/components/photo/photo.js
+++ client/src/components/photo/photo.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { CardMedia } from "@material-ui/core";
+import ProfilePic from "./profilepic.jpg";
 
-function Photo () {
-  return(
-    <div>
-      Photo
-    </div>
+function Photo() {
+  return (
+    <CardMedia
+      component="img"
+      src={ProfilePic}
+      title="Coleman Buffa Portrait"
+    />
   );
 }
 
