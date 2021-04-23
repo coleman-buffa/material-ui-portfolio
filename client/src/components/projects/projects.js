8214a2bc59b97912c90cf86c9663930a769b25d0
Coleman Buffa <coleman.buffa@gmail.com>
2021-04-23 10:44:51 -0700

    Bring skills array to front end and display on page.

--- client/src/components/projects/projects.js
+++ client/src/components/projects/projects.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect}  from 'react';
+import React, { useState, useEffect } from 'react';
 import API from "../../utils/API";
 import Skills from "../skills/skills";
 import ProjectCard from "../projectcard/projectcard";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
 
-function Projects () {
+function Projects() {
 
   const [projects, setProjects] = useState([]);
   const [skills, setSkills] = useState([]);
@@ -14,25 +16,31 @@ function Projects () {
   }, []);
 
   const loadProjects = () => {
-    API.getProjects({})
-      .then(data => {        
+    API.getProjects()
+      .then(data => {
         setProjects(data.data);
       })
   };
 
   const loadSkills = () => {
-    //Scan through project.skills and create a unique entry for each skill 
+    API.getSkills()
+      .then(data => {
+        setSkills(data.data);
+      })
   }
 
-//State goes here
-  //State to track all project objects fetched from DB array of objects
-  //State to track all unique skills pulled from projects
-
-
-  return(
+  return (
     <div>
-      <Skills/>
-      <ProjectCard/>
+      <Grid container spacing={2}>
+        {skills.map(skill => (
+          <Skills
+          key={skill}  
+          skill={skill}
+          />
+        ))}
+
+      </Grid>
+      <ProjectCard />
     </div>
   );
 }
